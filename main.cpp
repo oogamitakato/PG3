e81c9fe3e7519f9222ae8295f43e6caa532a31f5
TakatoOogami <k021g1156@g.neec.ac.jp>
2022-11-27 14:33:47 +0900

    ラムダ式修正

--- main.cpp
+++ main.cpp
@@ -4,14 +4,38 @@
 #include <Windows.h>
 #include <functional>
 
+typedef void (*PFunc)(int*, int*);
+
+//コールバック関数
+void DisplayResult(int* s, int* num) {
+	//結果を表示
+	printf("%d! ", *num);
+	if (*num % 2 == 0) {
+		printf("丁!\n");
+	}
+	else {
+		printf("半!\n");
+	}
+
+}
+
+void SetTimeOut(PFunc p, int second, int num) {
+	//コールバック関数を呼び出す
+	Sleep(second * 1000);
+
+	p(&second, &num);
+}
+
 int main(int argc, const char* argv[])
 {
+	PFunc p = DisplayResult;
+	int diceNum = rand() % 6 + 1;
+	int number = 0;
+
 	//サイコロの目をランダムに決める
 	srand(time(nullptr));
-	int diceNum = rand() % 6 + 1;
 
 	//半か丁かを入力
-	int number = 0;
 	printf("半か丁かを入力\n1...半 2...丁\n");
 	scanf_s("%d", &number);
 
@@ -30,23 +54,12 @@ int main(int argc, const char* argv[])
 	printf("正解は...\n");
 
 	//ラムダ式
-	std::function<int(int)> fx = [=](int s) {
-		//数秒待つ
-		Sleep(s * 1000);
-
-		//結果を表示
-		printf("%d! ", diceNum);
-		if (diceNum % 2 == 0) {
-			printf("丁!\n");
-		}
-		else {
-			printf("半!\n");
-		}
-
+	std::function<void(PFunc,int,int)> result = [=](PFunc p,int num,int second) {	
+		SetTimeOut(p, second, num);
 		return 0;
 	};
 
-	fx(3);
+	result(p, diceNum, 3);
 
 	//勝敗判定
 	if (number % 2 == diceNum % 2) {
