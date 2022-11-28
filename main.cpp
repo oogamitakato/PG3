89895286e676fd3fbc5552c216f4540bbe939611
TakatoOogami <k021g1156@g.neec.ac.jp>
2022-11-28 10:14:45 +0900

    双方向リスト実装

--- main.cpp
+++ main.cpp
@@ -5,67 +5,84 @@
 const int wordCount = 16;
 
 typedef struct cell{
-	char str[wordCount];
-	int loanDate;
-	int returnDate;
+	int val;
+	struct cell* prev;
 	struct cell* next;
 } CELL;
 
-void create(CELL *endCell, const char *buf, int ldate, int rdate){
-	//新規作成するセル
-	CELL* newCell;
-	//新規作成するセル分のメモリを確保
-	newCell = (CELL*)malloc(sizeof(CELL));
-
-	strcpy_s(newCell->str, wordCount, buf);
-	newCell->loanDate = ldate;
-	newCell->returnDate = rdate;
-	newCell->next = nullptr;
-
-	while (endCell->next != nullptr) {
-		endCell = endCell->next;
-	}
-
-	endCell->next = newCell;
-
-};
-
-void index(CELL *firstCell) {
-	while (firstCell->next != nullptr) {
-		firstCell = firstCell->next;
-		printf("%s,%d,%d\n", firstCell->str,firstCell->loanDate,firstCell->returnDate);
-	}
-	printf("\n");
-};
+void create(CELL* currentCell, int val);
+void index(CELL* firstCell);
+CELL* getInsertListAddress(CELL *endCELL, int iteretor);
 
 int main()
 {
-	char str[wordCount];
-	int loanDtae = 0;
-	int returnDtae = 0;
-	int count = 0;
+	int iteretor;
+	int inputValue;
+	CELL* insertCell;
 
 	CELL head;
 	head.next = nullptr;
+	head.prev = nullptr;
 
 	while (true) {
-		printf("氏名 : ");
-		scanf_s("%s", str, wordCount);
-		printf("貸出日 : ");
-		scanf_s("%d", &loanDtae);
-		printf("返却日 : ");
-		scanf_s("%d", &returnDtae);
+		printf("何番目のセルの後ろに挿入しますか\n");
+		scanf_s("%d", &iteretor);
+
+		printf("挿入する値を入力してください\n");
+		scanf_s("%d", &inputValue);
+	
 		printf("\n");
 
-		//最後尾にセルを追加
-		create(&head, str, loanDtae, loanDtae);
+		//任意のセルの後ろに追加
+		insertCell = getInsertListAddress(&head, iteretor);
+		create(insertCell, inputValue);
+
 		//リスト一覧の表示
 		printf("リスト一覧\n");
 		index(&head);
 		printf("\n");
-
-		count++;
 	}
 
 	return 0;
+}
+
+void create(CELL* currentCell, int val) {
+	//新規作成するセル
+	CELL* newCell;
+	//新規作成するセル分のメモリを確保
+	newCell = (CELL*)malloc(sizeof(CELL));
+	newCell->val = val;
+	newCell->prev = currentCell;
+	newCell->next = currentCell->next;
+
+	if (currentCell->next) {
+		CELL *nextCell = currentCell->next;
+		nextCell->prev = newCell;
+	}
+
+	currentCell->next = newCell;
+
+};
+
+void index(CELL* endCell) {
+	int no = 1;
+	while (endCell->next != nullptr) {
+		endCell = endCell->next;
+		printf("%d : ", no);
+		printf("%d\n", endCell->val);
+
+		no++;
+	}
+};
+
+CELL* getInsertListAddress(CELL* endCELL, int iteretor) {
+	for (int i = 0; i < iteretor; i++) {
+		if (endCELL->next) {
+			endCELL = endCELL->next;
+		}
+		else {
+			break;
+		}
+	}
+	return endCELL;
 }
