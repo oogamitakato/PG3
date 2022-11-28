1c9e79e01da87b83e919decd7862576a6743bfe8
TakatoOogami <k021g1156@g.neec.ac.jp>
2022-11-28 09:28:31 +0900

    02_02実装

--- main.cpp
+++ main.cpp
@@ -1,51 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+const int wordCount = 16;
 
 typedef struct cell{
-	int val;
+	char str[wordCount];
+	int loanDate;
+	int returnDate;
 	struct cell* next;
 } CELL;
 
-void create(CELL *firstCell, int insertVal){
+void create(CELL *endCell, const char *buf, int ldate, int rdate){
 	//新規作成するセル
-	CELL* cell;
+	CELL* newCell;
 	//新規作成するセル分のメモリを確保
-	cell = (CELL*)malloc(sizeof(CELL));
+	newCell = (CELL*)malloc(sizeof(CELL));
 
-	cell->val = insertVal;
-	cell->next = nullptr;
+	strcpy_s(newCell->str, wordCount, buf);
+	newCell->loanDate = ldate;
+	newCell->returnDate = rdate;
+	newCell->next = nullptr;
 
-	while (firstCell->next != nullptr) {
-		firstCell = firstCell->next;
+	while (endCell->next != nullptr) {
+		endCell = endCell->next;
 	}
 
-	firstCell = cell;
+	endCell->next = newCell;
 
 };
 
 void index(CELL *firstCell) {
 	while (firstCell->next != nullptr) {
 		firstCell = firstCell->next;
-		printf("%d\n", firstCell->val);
+		printf("%s,%d,%d\n", firstCell->str,firstCell->loanDate,firstCell->returnDate);
 	}
+	printf("\n");
 };
 
 int main()
 {
-	int val;
+	char str[wordCount];
+	int loanDtae = 0;
+	int returnDtae = 0;
 	int count = 0;
 
 	CELL head;
 	head.next = nullptr;
 
-	while (count < 5) {
-		scanf_s("%d", &val);
+	while (true) {
+		printf("氏名 : ");
+		scanf_s("%s", str, wordCount);
+		printf("貸出日 : ");
+		scanf_s("%d", &loanDtae);
+		printf("返却日 : ");
+		scanf_s("%d", &returnDtae);
+		printf("\n");
 
 		//最後尾にセルを追加
-		create(&head, val);
-
+		create(&head, str, loanDtae, loanDtae);
 		//リスト一覧の表示
+		printf("リスト一覧\n");
 		index(&head);
+		printf("\n");
 
 		count++;
 	}
